37d3b8b912906cbb5235c5e974b452c635ace844
Vineet <vineetkushwaha6325@gmail.com>
2025-07-06 16:09:34 +0530

    2.0 icon and hero start

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+
+        <link rel="icon" href="/icon.png" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <AudioProvider>
           <Analytics />
