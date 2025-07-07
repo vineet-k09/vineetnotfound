3e1945239febf2ac47c80a4dda28b37b56260a34
Vineet <vineetkushwaha6325@gmail.com>
2025-07-08 01:27:51 +0530

    2.1 navbar -- near done

--- src/app/components/narbar.tsx
+++ src/app/components/narbar.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { motion, useScroll, useTransform, useSpring } from 'framer-motion';
 
 import { useLangContext } from '../../hooks/useLang';
 
@@ -7,6 +8,17 @@ export default function Navbar() {
     const [time, setTime] = useState('');
     const [temp, setTemp] = useState('--°C');
     const { visibleText } = useLangContext();
+    const { scrollY } = useScroll();
+
+    const smoothScroll = useSpring(scrollY, {
+        damping: 40,
+        stiffness: 500,
+    })
+
+    const width = useTransform(smoothScroll, [0, 300], ['100vw', '70vw'])
+    const top = useTransform(smoothScroll, [0, 300], ['-1px', '8px'])
+    const bRadius = useTransform(smoothScroll, [0, 300], ['0px', '40px'])
+    const fontSize = useTransform(smoothScroll, [0, 300], ['clamp(1.2rem, 5vw, 2.5rem)', 'clamp(1rem, 4vw, 2rem)']);
 
     // Update time every second
     useEffect(() => {
@@ -44,19 +56,33 @@ export default function Navbar() {
     }, []);
 
     return (
-        <nav className="border-b container">
-            <div className="grid-layout">
-                <div className="content-area justify-between items-center flex text-center my-3 ">
-                    <h3>{visibleText.name}</h3>
-
-                    <ul className="list-none flex gap-5">
-                        <li className='text-sm border-2 min-w-18 py-1 rounded-lg'>{isTime ? time : temp}</li>
-                        <li className='py-1'><a href="#">Home</a></li>
-                        <li className='py-1'><a href="#">Projects</a></li>
-                        <li className='py-1'><a href="#">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <>
+            <motion.div
+                style={{
+                    width: width,
+                    top: top,
+                    borderRadius: bRadius,
+                }}
+                className="navbar backdrop-blur-sm fixed">
+                <nav className="container">
+                    <div className="grid-layout">
+                        <div className="content-area justify-between items-center flex text-center my-3 ">
+                            <motion.h3
+                                style={{
+                                    fontSize: fontSize,
+                                }}
+                            >{visibleText.name}</motion.h3>
+                            {/* <div className="burger block sm:hidden"></div> */}
+                            <ul className="list-none hidden sm:flex gap-5">
+                                <li className='text-sm border-2 min-w-18 py-1 rounded-lg'>{isTime ? time : temp}</li>
+                                <li className='py-1'><a href="#">Home</a></li>
+                                <li className='py-1'><a href="#">Projects</a></li>
+                                <li className='py-1'><a href="#">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </motion.div>
+        </>
     );
 }
