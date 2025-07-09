1cf2fb23a078b94b3e005f5091bd2bc7e9573387
Vineet <vineetkushwaha6325@gmail.com>
2025-07-10 00:05:30 +0530

    3.0 responsive desgin of new pages

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,18 +5,16 @@ import PageWrapper from '../components/utility/pageWrapper'
 export default function Contact() {
     return (
         <PageWrapper>
-            <main className="flex-grow container mt-10">
-                <div className="grid-layout">
-                    <div className="content-area sm:px-10">
-                        <h1 className='my-10'>Contact</h1>
-                        <div className="grid grid-cols-3">
+            <div className="grid-layout">
+                <div className="content-area sm:px-10 flex justify-center flex-col">
+                    <h2 className='text-center my-10'>Contact</h2>
+                    <section className='section'>
                             <a href='https://github.com/vineet-k09' target='_blank'><i className="devicon devicon-github-original hover:text-[var(--accent)]"></i></a>
                             <a href='https://www.linkedin.com/in/vineet-kushwaha-2666b5257/' target='_blank'><i className="devicon devicon-linkedin-plain hover:text-[var(--accent)]"></i></a>
                             <a href='mailTo:vineetkushwaha6325@gmail.com'><i className="fa-solid fa-envelope hover:text-[var(--accent)]"></i></a>
-                        </div>
-                    </div>
+                    </section>
                 </div>
-            </main>
+            </div>
         </PageWrapper >
     )
 }
