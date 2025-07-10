aa26735a0ce016644dc14c949fcf9bcdbaa401b0
Vineet <vineetkushwaha6325@gmail.com>
2025-07-10 20:42:29 +0530

    3.5 ---.---

--- src/app/components/render/carousel.tsx
+++ src/app/components/render/carousel.tsx
@@ -35,13 +35,13 @@ export default function ProjectCarousel({ images = [] }: { images: string[] }) {
                     <div
                         key={i}
                         id= {i.toString()}
-                        className="relative shrink-0 rounded-xl overflow-hidden w-[80vw] sm:w-[50vw] h-[300px] flex-none"
+                        className="relative shrink-0 rounded-xl overflow-hidden w-[80vw] sm:w-[30vw] h-[300px] md:h-[300px] sm:h-[350px] flex-none"
                     >
                         <Image
                             src={src}
                             alt={`Screenshot ${i + 1}`}
                             layout="fill"
-                            className="rounded-xl //sm:object-cover"
+                            className="rounded-xl sm:object-cover"
                         />
                     </div>
                 ))}
